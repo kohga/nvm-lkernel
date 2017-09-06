545912b6c33199efb9249b21a2937c39eaa3f6c7
kohga <khkhg92lib.ggl@gmail.com>
2017-09-06 19:57:23 +0900

    dayend

--- linux-3.12.49/include/generated/compile.h
+++ linux-3.12.49/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 3 */
 /* SMP */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#3 SMP Mon Sep 4 11:09:59 JST 2017"
+#define UTS_VERSION "#3 SMP Wed Sep 6 19:48:44 JST 2017"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "HAGAs-Ubuntu"
 #define LINUX_COMPILER "gcc version 5.4.0 20160609 (Ubuntu 5.4.0-6ubuntu1~16.04.4) "
