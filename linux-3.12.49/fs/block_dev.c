e525e083efa6c76bafe8ce4f52072ca2d864153e
kohga <khkhg92lib.ggl@gmail.com>
2017-10-15 22:43:39 +0900

    Add printk

--- linux-3.12.49/fs/block_dev.c
+++ linux-3.12.49/fs/block_dev.c
@@ -502,7 +502,7 @@ struct block_device *bdget(dev_t dev)
 			bdev_test, bdev_set, &dev);
 
 	if (!inode){
-		printk("kohga; bdget; NULL\n");
+		printk(KERN_DEBUG "kohga; bdget; NULL\n");
 		return NULL;
 	}
 
@@ -721,14 +721,17 @@ static struct block_device *bd_start_claiming(struct block_device *bdev,
 	int partno, err;
 
 	might_sleep();
+	printk(KERN_DEBUG "kohga; bd_start_claiming\n");
 
 	/*
 	 * @bdev might not have been initialized properly yet, look up
 	 * and grab the outer block device the hard way.
 	 */
 	disk = get_gendisk(bdev->bd_dev, &partno);
-	if (!disk)
+	if (!disk){
+		printk(KERN_DEBUG "kohga;  disk is NULL\n");
 		return ERR_PTR(-ENXIO);
+	}
 
 	/*
 	 * Normally, @bdev should equal what's returned from bdget_disk()
@@ -738,15 +741,20 @@ static struct block_device *bd_start_claiming(struct block_device *bdev,
 	 * tracking is broken for those devices but it has always been that
 	 * way.
 	 */
-	if (partno)
+	if (partno){
+		printk(KERN_DEBUG "kohga;  partno = %d\n",partno);
 		whole = bdget_disk(disk, 0);
-	else
+	}else{
+		printk(KERN_DEBUG "kohga;  partno is NULL \n");
 		whole = bdgrab(bdev);
+	}
 
 	module_put(disk->fops->owner);
 	put_disk(disk);
-	if (!whole)
+	if (!whole){
+		printk(KERN_DEBUG "kohga; bd;  whole is NULL\n",whole);
 		return ERR_PTR(-ENOMEM);
+	}
 
 	/* prepare to claim, if successful, mark claiming in progress */
 	spin_lock(&bdev_lock);
@@ -755,10 +763,12 @@ static struct block_device *bd_start_claiming(struct block_device *bdev,
 	if (err == 0) {
 		whole->bd_claiming = holder;
 		spin_unlock(&bdev_lock);
+		printk(KERN_DEBUG "kohga; return  whole = %d\n",whole);
 		return whole;
 	} else {
 		spin_unlock(&bdev_lock);
 		bdput(whole);
+		printk(KERN_DEBUG "kohga; return err whole = %d\n",whole);
 		return ERR_PTR(err);
 	}
 }
@@ -1227,12 +1237,20 @@ int blkdev_get(struct block_device *bdev, fmode_t mode, void *holder)
 		whole = bd_start_claiming(bdev, holder);
 		if (IS_ERR(whole)) {
 			bdput(bdev);
-			printk("kohga;  blkdev_get; ERR_PTR(whole)\n");
+			printk(KERN_DEBUG "kohga;  blkdev_get; ERR_PTR(whole)\n");
 			return PTR_ERR(whole);
 		}
 	}
 
 	res = __blkdev_get(bdev, mode, 0);
+	if(!res){
+		printk(KERN_DEBUG "kohga;  res is NULL\n");
+	}else{
+		printk(KERN_DEBUG "kohga;  res\n");
+	}
+
+	printk(KERN_DEBUG "kohga;  res = %d\n",res);
+	printk(KERN_DEBUG "kohga;  whole = %d\n",whole);
 
 	if (whole) {
 		struct gendisk *disk = whole->bd_disk;
@@ -1241,6 +1259,7 @@ int blkdev_get(struct block_device *bdev, fmode_t mode, void *holder)
 		mutex_lock(&bdev->bd_mutex);
 		spin_lock(&bdev_lock);
 
+		printk(KERN_DEBUG "kohga;  res2\n");
 		if (!res) {
 			BUG_ON(!bd_may_claim(bdev, whole, holder));
 			/*
@@ -1253,6 +1272,7 @@ int blkdev_get(struct block_device *bdev, fmode_t mode, void *holder)
 			whole->bd_holder = bd_may_claim;
 			bdev->bd_holders++;
 			bdev->bd_holder = holder;
+			printk(KERN_DEBUG "kohga;  res3\n");
 		}
 
 		/* tell others that we're done */
@@ -1273,11 +1293,14 @@ int blkdev_get(struct block_device *bdev, fmode_t mode, void *holder)
 		    (disk->flags & GENHD_FL_BLOCK_EVENTS_ON_EXCL_WRITE)) {
 			bdev->bd_write_holder = true;
 			disk_block_events(disk);
+			printk(KERN_DEBUG "kohga;  res4\n");
 		}
 
 		mutex_unlock(&bdev->bd_mutex);
 		bdput(whole);
 	}
+
+	printk(KERN_DEBUG "kohga; return  res!! \n");
 
 	return res;
 }
@@ -1352,13 +1375,13 @@ struct block_device *blkdev_get_by_dev(dev_t dev, fmode_t mode, void *holder)
 
 	bdev = bdget(dev);
 	if (!bdev){
-		printk("kohga;  ERR_PTR(-ENOMEM)\n");
+		printk(KERN_DEBUG "kohga;  ERR_PTR(-ENOMEM)\n");
 		return ERR_PTR(-ENOMEM);
 	}
 
 	err = blkdev_get(bdev, mode, holder);
 	if (err){
-		printk("kohga;  ERR_PTR(err)\n");
+		printk(KERN_DEBUG "kohga;  ERR_PTR(err)  err = %d \n",err);
 		return ERR_PTR(err);
 	}
 
--- linux-3.12.49/include/generated/compile.h
+++ linux-3.12.49/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 65 */
+/* This file is auto generated, version 68 */
 /* SMP */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#65 SMP Fri Oct 13 21:28:14 JST 2017"
+#define UTS_VERSION "#68 SMP Sun Oct 15 22:08:38 JST 2017"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "kohga-ubuntu"
 #define LINUX_COMPILER "gcc version 5.4.0 20160609 (Ubuntu 5.4.0-6ubuntu1~16.04.4) "
