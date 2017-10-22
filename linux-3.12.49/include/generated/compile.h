27e0f407f0b1f2bbeb08165d8d7f073cb11eddd4
kohga <khkhg92lib.ggl@gmail.com>
2017-10-23 08:10:58 +0900

    Delete printk(VFS)

--- linux-3.12.49/include/generated/compile.h
+++ linux-3.12.49/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 76 */
+/* This file is auto generated, version 78 */
 /* SMP */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#76 SMP Fri Oct 20 22:53:30 JST 2017"
+#define UTS_VERSION "#78 SMP Mon Oct 23 07:54:20 JST 2017"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "kohga-ubuntu"
 #define LINUX_COMPILER "gcc version 5.4.0 20160609 (Ubuntu 5.4.0-6ubuntu1~16.04.4) "
